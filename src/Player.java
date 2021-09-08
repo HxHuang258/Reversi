b1059cc1301608581a782ae6260ba75bd6f748b1
HxHuang258 <harryhuang258@gmail.com>
2021-09-08 22:45:00 +0100

    Rename variable move to turn

--- src/Player.java
+++ src/Player.java
@@ -2,7 +2,6 @@
 public class Player
 {
 	int player; //player 1 (black) or player 2 (white)
-	boolean move; //true if it's the players move
+	boolean turn; //true if it's the players move
 	Frame window; //have it's own display window
-	
 }
